6ef77a79625f3ac9093910f1fd89336e6c5dfef3
Cristina Marques <ccmarques98@gmail.com>
2024-09-26 15:14:35 +0200

    get and post products

--- src/App.js
+++ src/App.js
@@ -1,21 +1,90 @@
 import "./App.css";
 import React from "react";
-const NODE_API_URL = process.env.REACT_APP_NODE_API_URL || "http://localhost:5000";
+import Product from "./components/product/product";
+const NODE_API_URL =
+  process.env.REACT_APP_NODE_API_URL || "http://localhost:5000";
 
 function App() {
-  const [message, setMessage] = React.useState("");
+  const [message, setMessage] = React.useState("Loading...");
+  const [products, setProducts] = React.useState([]);
+  const [error, setError] = React.useState("null");
 
   React.useEffect(() => {
-    fetch(NODE_API_URL)
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error(error));
+    fetch(`${NODE_API_URL}/api/status`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMessage(`✅ ${data.message}`);
+      })
+      .catch((error) => {
+        setMessage("⚠️ Error: " + error);
+      });
+
+    fetchProducts();
   }, []);
+
+  const submitProduct = (event) => {
+    event.preventDefault();
+    const form = event.target.form;
+    setError("no error");
+    const name = form.name.value;
+    const price = form.price.value;
+
+    fetch(`${NODE_API_URL}/api/products`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, price }),
+    })
+      .catch((error) => {
+        setError("Error adding product: " + error);
+      })
+      .then((res) => res.json())
+      .then(() => {
+        fetchProducts();
+      });
+  };
+
+  const fetchProducts = () => {
+    fetch(`${NODE_API_URL}/api/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+      })
+      .catch((error) => {
+        console.error("Error fetching products: ", error);
+      });
+  };
 
   return (
     <div className="App">
-      <h1>Shop App</h1>
-      <p>{message}</p>
+      <h1>My shopping list</h1>
+      <p>Server status: {message}</p>
+      <hr />
+      {!products && <p>No products found</p>}
+      {products && (
+        <div>
+          <h1>Product Gallery</h1>
+          <div className="gallery">
+            {products.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
+          <form>
+            <label htmlFor="name">Name:</label>
+            <input id="name" type="text" name="name" />
+            <br />
+            <label htmlFor="price">Price:</label>
+            <input id="price" type="number" name="price" />
+            <br />
+            <button type="submit" onClick={submitProduct}>
+              Add product
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,8 +1,18 @@
 import React from "react";
 
+const styles = {
+  product: {
+    border: "1px solid black",
+    padding: "10px",
+    margin: "10px",
+    width: "200px",
+    display: "inline-block",
+  },
+};
+
 const Product = ({ product }) => {
   return (
-    <div className="product">
+    <div className="product" style={styles.product}>
       <h2>{product.name}</h2>
       <p>${product.price}</p>
     </div>
